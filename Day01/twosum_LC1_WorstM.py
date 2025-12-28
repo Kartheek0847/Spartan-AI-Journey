e35044a39b28e42e03e78e3efa8d971bdde8a201
VATTIKUNTA KARTHEEK KUMAR <karthikkumar0847@gmail.com>
2025-12-28 16:53:16 +0530

    Fix method signature in twonums function

--- Day01/twosum_LC1_WorstM.py
+++ Day01/twosum_LC1_WorstM.py
@@ -1,6 +1,7 @@
 class solution(object):
- def twonums(nums,target,self):
+ def twonums(self,nums,target):
      for i in range(0,len(nums)):
             for j in range(i+1,len(nums)):
                 if nums[i] + nums[j] == target:
+
                     return [i,j]
